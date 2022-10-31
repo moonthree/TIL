de09d041eacfd0f21cf5907e7f3405c2411ec693
moonthree <fpdhslr0725@gmail.com>
2022-10-31 17:59:26 +0900

    a

--- woote/weak1/7.js
+++ woote/weak1/7.js
@@ -43,20 +43,34 @@ function problem7(user, friends, visitors) {
         recommendFriend.score += 1
       }
     })
-
     friends.forEach((friend) => {
-      friend.forEach((element) => {
-        if (usersFriend.includes(element)) {
-          
-        }
-      })
+      // console.log(friend)
+      if (recommendFriend.name === friend[1]) {
+        recommendFriend.score += 10
+      }
     })
+
   })
 
   // 함께 아는 친구의 수로 추천 점수 추가
-  recommendFriends.forEach((element) => {
-    console.log(element)
-  })
+  // recommendFriends.forEach((element) => {
+  //   console.log(element)
+  // })
+
+  recommendFriends.sort((x, y) => y.score - x.score)
+
+
+  answer = []
+  for (let i = 0; i < recommendFriends.length; i++) {
+    if (i >= 5) {
+      break
+    }
+    if (recommendFriends[i].score > 0) {
+      answer.push(recommendFriends[i].name)
+    }
+  }
+
+  console.log(answer)
 
   // object 추천 점수로 정렬
   // const recommendFriendSort = Object.fromEntries(
